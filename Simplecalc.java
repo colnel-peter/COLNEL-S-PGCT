c0cf76a404bebea2373452177d7514da11fca7be
colnel-peter <colnel957@gmail.com>
2024-10-29 17:09:22 +0300

    second commit

---
+++ Simplecalc.java
@@ -0,0 +1,13 @@
+public class Simplecalc {
+    public static void multiplyNumbers(){
+        int num1=8;
+        int num2=6;
+        int answer=num1*num2;
+        System.out.println(answer);
+
+    }
+    public static void main(String[]args){
+        multiplyNumbers();
+    }
+    
+}
